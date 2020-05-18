ae6fdef38fb0208b15bba3314fc0b6a0261e15a1
KathleenKu <64280133+KathleenKu@users.noreply.github.com>
2020-05-18 17:47:39 +0200

    changes

--- my-database/my-database.sql
+++ my-database/my-database.sql
@@ -12,11 +12,10 @@ INSERT INTO `infects` (`date`,`cases`,`death`,`country`) VALUES ('02.05.2020','1
 
 
 CREATE Table `dax` (
-    
     `date` date NOT NUll,
     `open` float(100) default NULL,
     `close` float(100) default NULL,
     `diff` float(100) default NULL,
 PRIMARY KEY  (`date`) ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-INSERT INTO `infects` ('daxvalue',`date`,`open`,`close`,`diff`) VALUE
+INSERT INTO `infects` (`date`,`open`,`close`,`diff`) VALUE
