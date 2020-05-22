90d9403f1aea9113df15459592f3f1ebf7de997d
FGeiselhart <f.geiselhart@outlook.de>
2020-05-22 23:18:56 +0200

    Modified sql Statement to fit values of Spark result

--- my-database/my-database.sql
+++ my-database/my-database.sql
@@ -8,15 +8,15 @@ CREATE Table `infects` (
     `continent` varchar(64) default NULL,
     `cases_prev` int default NULL,
     `dead_prev` int default NULL,
-    `cases_rel_diff` decimal(6,4) default NULL,
-    `deaths_rel_diff` decimal(6,4) default NULL,
+    `cases_rel_diff` decimal(10,4) default NULL,
+    `deaths_rel_diff` decimal(10,4) default NULL,
 PRIMARY KEY  (`date`, `country`) ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 INSERT INTO `infects` (`date`,`cases`,`deaths`,`country`,`continent`, `cases_prev`, `dead_prev`, `cases_rel_diff`, `deaths_rel_diff`) VALUES ('02.05.2020','164','17000','Germany', 'Europe', NULL, NULL, NULL, NULL);
 
 CREATE Table `dax` (
     `date` date NOT NUll,
-    `open` decimal(6,4) default NULL,
-    `close` decimal(6,4) default NULL,
-    `diff` decimal(6,4) default NULL,
+    `open` decimal(10,4) default NULL,
+    `close` decimal(10,4) default NULL,
+    `diff` decimal(10,4) default NULL,
 PRIMARY KEY  (`date`) ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
