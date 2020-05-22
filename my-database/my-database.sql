870a977c05eb0bca5c9af1a5fc0ae9a51fc40f8e
FGeiselhart <f.geiselhart@outlook.de>
2020-05-22 21:56:48 +0200

    Modified sql Statement to fit values of Spark result

--- my-database/my-database.sql
+++ my-database/my-database.sql
@@ -12,7 +12,7 @@ CREATE Table `infects` (
     `deaths_rel_diff` decimal(6,4) default NULL,
 PRIMARY KEY  (`date`) ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---INSERT INTO `infects` (`date`,`cases`,`death`,`country`,`continent`, `cases_prev`, `dead_prev`, `cases_rel_diff`, `deaths_rel_diff`) VALUES ('02.05.2020','164','17000','Germany', 'Europe', NULL, NULL, NULL, NULL);
+INSERT INTO `infects` (`date`,`cases`,`death`,`country`,`continent`, `cases_prev`, `dead_prev`, `cases_rel_diff`, `deaths_rel_diff`) VALUES ('02.05.2020','164','17000','Germany', 'Europe', NULL, NULL, NULL, NULL);
 
 CREATE Table `dax` (
     `date` date NOT NUll,
